d0f69e6fd29432620fb426ca6a9f114dd1cdc666
amaybaum <amaybaum@users.noreply.github.com>
2024-02-26 12:40:50 +0200

    Update App.java

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -1,10 +1,13 @@
 package com.houarizegai.calculator;
+import org.apache.log4j.LogManager;
 
+import org.apache.log4j.Logger;
 import com.houarizegai.calculator.ui.CalculatorUI;
 
 public class App {
-
+    protected static final Logger logger = LogManager.getLogger(App.class);
     public static void main(String[] args) {
+        logger.info("starting calculator app...");
         new CalculatorUI();
     }
 }
